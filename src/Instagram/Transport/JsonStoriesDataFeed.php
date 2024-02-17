75b6c83c7163873bfcbd22a30ba9b93ae6507599
Charly <charly.strn@gmail.com>
2024-02-17 14:01:38 +0100

    add

--- src/Instagram/Transport/JsonStoriesDataFeed.php
+++ src/Instagram/Transport/JsonStoriesDataFeed.php
@@ -31,7 +31,7 @@ class JsonStoriesDataFeed extends AbstractDataFeed
         ];
 
         //$endpoint = InstagramHelper::URL_BASE . 'graphql/query/?query_hash=' . InstagramHelper::QUERY_HASH_STORIES . '&variables=' . json_encode($variables);
-        $endpoint = InstagramHelper::URL_BASE . 'graphql/query/query_id=9957820854288654&user_id='.$int.'&include_chaining=false&include_reel=true&include_suggested_users=false&include_logged_out_extras=false&include_live_status=false&include_highlight_reels=true';
+        $endpoint = InstagramHelper::URL_BASE . 'graphql/query/?query_id=9957820854288654&user_id='.$int.'&include_chaining=false&include_reel=true&include_suggested_users=false&include_logged_out_extras=false&include_live_status=false&include_highlight_reels=true';
 
         $data = $this->fetchJsonDataFeed($endpoint);
 
